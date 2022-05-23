9bdb9caa8074058f35aa20942beaa11eca72ee88
mireu-san <67046863+mireu-san@users.noreply.github.com>
2022-05-23 20:57:29 +0900

    temporary - establishing toggle menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { MotionConfig } from 'framer-motion';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const close = () => setModalOpen(false)
+  const open = () => setModalOpen(true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="save-button"
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        Launch modal
+      </motion.button>
     </div>
   );
 }
 
+{/* <AnimatePresence
+    // Disable any initial animations on children that
+    // are present when the component is first rendered
+    initial={false}
+    // Only render one component at a time.
+    // The exiting component will finish its exit
+    // animation before entering component is rendered
+    exitBeforeEnter={true}
+    // Fires when all exiting nodes have completed animating out
+    onExitComplete={() => null}
+>
+    {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+</AnimatePresence>
+
+const flip = {
+  hidden: {
+    transform: "scale(0) rotateX(-360deg)",
+    opacity: 0,
+    transition: {
+      delay: 0.3,
+    },
+  },
+  visible: {
+    transform: " scale(1) rotateX(0deg)",
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    transform: "scale(0) rotateX(360deg)",
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}; */}
 export default App;
